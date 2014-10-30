d393e1410f3144aa50ba542e7eeb86a7b2624f7f
robertn702 <robertn702@gmail.com>
2014-10-30 16:27:25 -0700

    completed linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,59 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+
+    if (list.head === null) {
+      list.head = makeNode(value);
+      list.tail = list.head;
+    } else if (list.head === list.tail) {
+      list.head.next = makeNode(value);
+      list.tail = list.head.next;
+    } else {
+      list.tail.next = makeNode(value);
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function(){
+    if (list.head === null) {
+      return null;
+    } else if (list.head === list.tail) {
+      var temp = list.head.value;
+      list.head = null;
+      list.tail = null;
+      return temp;
+    } else {
+      var temp = list.head.value;
+      list.head = list.head.next;
+      return temp;
+    }
+
   };
 
-  list.contains = function(target){
+  list.contains = function(target, checkNode){
+    if (typeof checkNode === "undefined") {
+      checkNode = list.head;
+    } else {
+      checkNode = checkNode;
+    }
+
+    if (checkNode.value === target) {
+      console.log(true);
+      return true;
+    } else if (checkNode.next !== null) {
+      return list.contains(target, checkNode.next);
+    }
+
+    console.log(false);
+    return false;
+
+    // while (checkNode) {
+    //   if (checkNode.value === target) {
+    //    return true;
+    //   }
+    //   checkNode = checkNode.next;
+    // }
+    // return false
   };
 
   return list;
