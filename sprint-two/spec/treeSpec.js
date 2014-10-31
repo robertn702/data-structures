060250048878b5f6375d9ecd0b153fcdd7e31532
robertn702 <robertn702@gmail.com>
2014-10-31 12:25:57 -0700

    completed tree

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -35,7 +35,12 @@ describe('tree', function() {
   it('should correctly detect nested children', function(){
     tree.addChild(5);
     tree.addChild(6);
+    tree.addChild(9);
+    tree.addChild(15);
     tree.children[0].addChild(7);
+    tree.children[0].addChild(8);
+    tree.children[2].addChild(8);
+    tree.children[1].addChild(8);
     tree.children[1].addChild(8);
     expect(tree.contains(7)).to.equal(true);
     expect(tree.contains(8)).to.equal(true);
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,21 +1,28 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
   return newTree;
 };
-
-
-
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children[this.children.length] = makeTree(value);
 };
 
 treeMethods.contains = function(target){
-
+  if (this.value === target) {
+    return true;
+  } else if (this.children.length !== 0) {
+    for (var i = 0; i < this.children.length; i++) {
+      if(this.children[i].contains(target)) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 
