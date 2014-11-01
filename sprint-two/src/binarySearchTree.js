73a1492a340f0c7d78a3a9d2da09d6a8d2f6e9bb
robertn702 <robertn702@gmail.com>
2014-10-31 20:52:30 -0700

    progrees on bst

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,48 @@
 var makeBinarySearchTree = function(value){
+  var tree = {};
+  if(value !== 'undefined') {
+    tree[value] = {};
+    tree[value].left = null;
+    tree[value].right = null;
+  }
+  _.extend(tree, bstMethods);
+  return tree;
 };
+
+var bstMethods = {};
+
+bstMethods.insert = function(value) {
+  var node = {};
+  node.left = null;
+  node.right = null;
+  var keys = Object.keys(this.tree);
+
+  //if anything in tree
+  // no then start tree
+  // yes then decide left or right
+  // repeat
+  if(keys.length === 0) {
+    this.tree[value] = node;
+  } else {
+
+  }
+
+
+};
+
+bstMethods.contains = function(value) {
+
+};
+
+bstMethods.depthFirstLog = function() {
+
+};
+
+
+
+
+
+
 
 
 /*
